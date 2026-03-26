c45f3271d6db332e81937889c2bcada5897ecef8
MingZhi Liu <xichen.dev@gmail.com>
2026-03-26 19:08:50 +0800

    Improve platformConsoleLog for better NSString handling
    Refactor platformConsoleLog to build log message as NSString.

--- shared/src/iosMain/kotlin/com/xichen/cloudphoto/core/logger/PlatformConsoleLog.ios.kt
+++ shared/src/iosMain/kotlin/com/xichen/cloudphoto/core/logger/PlatformConsoleLog.ios.kt
@@ -1,13 +1,29 @@
 package com.xichen.cloudphoto.core.logger
 
 import platform.Foundation.NSLog
+import platform.Foundation.NSString
 
+/**
+ * `NSLog` 是可变参数 Objective-C API：把 Kotlin [String] 直接当作 `%@` 传入时，桥接到 `NSString` 不可靠，
+ * 容易在运行时触发 EXC_BREAKPOINT。应先转为 [NSString]（Kotlin/Native 对 Apple 目标的显式转换）。
+ */
 internal actual fun platformConsoleLog(
     level: LogLevel,
     tag: String,
     message: String,
     throwable: Throwable?
 ) {
-    NSLog("%@", "[${level.name.first()}][$tag] $message")
-    throwable?.printStackTrace()
+    val line = buildString {
+        append('[')
+        append(level.name.first())
+        append("][")
+        append(tag)
+        append("] ")
+        append(message)
+        if (throwable != null) {
+            append('\n')
+            append(throwable.stackTraceToString())
+        }
+    }
+    NSLog("%@", line as NSString)
 }
